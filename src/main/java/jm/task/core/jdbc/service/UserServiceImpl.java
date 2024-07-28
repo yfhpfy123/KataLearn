c7364a493c2a7cf1bd813554d7e08001492aadd4
yfhpfy123 <ivash_max@mail.ru>
2024-07-28 06:07:16 +0300

    Add Hibernate

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoJDBCImpl jdbc = new UserDaoJDBCImpl();
+    private UserDao jdbc = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
         jdbc.createUsersTable();
