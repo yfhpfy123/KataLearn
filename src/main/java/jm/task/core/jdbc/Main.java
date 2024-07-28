74d953d57f1e1663c9122f81573b41c1a30ca24b
yfhpfy123 <ivash_max@mail.ru>
2024-07-28 06:14:37 +0300

    JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,42 +9,42 @@ public class Main {
         // реализуйте алгоритм здесь
         UserDao userDao = new UserDaoJDBCImpl();
         UserDao userDaoH = new UserDaoHibernateImpl();
-//        System.out.println("--------------------JDBC-------------------");
-//
-//        // JDBC блок
-//        userDao.createUsersTable();
-//        System.out.println();
-//
-//        userDao.saveUser("Петя", "Петин", (byte) 20);
-//        userDao.saveUser("Вася", "Васин", (byte) 12);
-//        userDao.saveUser("Коля", "Колин", (byte) 25);
-//        userDao.saveUser("Женя", "Женин", (byte) 18);
-//        System.out.println();
-//
-//        userDao.removeUserById(2);
-//        System.out.println(userDao.getAllUsers());
-//        System.out.println();
-//
-//        userDao.cleanUsersTable();
-//        userDao.dropUsersTable();
+        System.out.println("--------------------JDBC-------------------");
+
+        // JDBC блок
+        userDao.createUsersTable();
+        System.out.println();
+
+        userDao.saveUser("Петя", "Петин", (byte) 20);
+        userDao.saveUser("Вася", "Васин", (byte) 12);
+        userDao.saveUser("Коля", "Колин", (byte) 25);
+        userDao.saveUser("Женя", "Женин", (byte) 18);
+        System.out.println();
+
+        userDao.removeUserById(2);
+        System.out.println(userDao.getAllUsers());
+        System.out.println();
+
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
         System.out.println("\n-------------------Hibernate-------------------");
 
         // Hibernate блок
         userDaoH.createUsersTable();
         System.out.println();
 
-//        userDaoH.saveUser("Hib_Петя", "Петин", (byte) 20);
-//        userDaoH.saveUser("Hib_Вася", "Васин", (byte) 12);
-//        userDaoH.saveUser("Hib_Коля", "Колин", (byte) 25);
-//        userDaoH.saveUser("Hib_Женя", "Женин", (byte) 18);
-//        System.out.println();
-//
-//        userDaoH.removeUserById(2);
-//        System.out.println(userDaoH.getAllUsers());
-//        System.out.println();
-//
-//        userDaoH.cleanUsersTable();
-//        userDaoH.dropUsersTable();
+        userDaoH.saveUser("Hib_Петя", "Петин", (byte) 20);
+        userDaoH.saveUser("Hib_Вася", "Васин", (byte) 12);
+        userDaoH.saveUser("Hib_Коля", "Колин", (byte) 25);
+        userDaoH.saveUser("Hib_Женя", "Женин", (byte) 18);
+        System.out.println();
+
+        userDaoH.removeUserById(2);
+        System.out.println(userDaoH.getAllUsers());
+        System.out.println();
+
+        userDaoH.cleanUsersTable();
+        userDaoH.dropUsersTable();
 
     }
 }
