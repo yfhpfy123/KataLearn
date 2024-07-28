c4fe556263283c854fd572384fb4d91c7e3c5cca
yfhpfy123 <ivash_max@mail.ru>
2024-07-28 06:09:20 +0300

    Add Hibernate

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,33 +1,35 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoJDBCImpl jdbc = new UserDaoJDBCImpl();
+    private UserDao hibernate = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
-        jdbc.createUsersTable();
+        hibernate.createUsersTable();
     }
 
     public void dropUsersTable() {
-        jdbc.dropUsersTable();
+        hibernate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        jdbc.saveUser(name, lastName, age);
+        hibernate.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        jdbc.removeUserById(id);
+        hibernate.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return jdbc.getAllUsers();
+        return hibernate.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        jdbc.cleanUsersTable();
+        hibernate.cleanUsersTable();
     }
 }
